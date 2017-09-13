973ba4949c080d238ae4a45b0090834914bec719
Alberto Morales <mail@albertomorales.eu>
2017-09-13 10:59:52 +0200

    ajustes para pasar de testng a junit, incluyendo el dataprovider. intro lista?

---
+++ src/test/java/eu/albertomorales/tddIntro/AppTest.java
@@ -0,0 +1,18 @@
+package eu.albertomorales.tddIntro;
+
+import junit.framework.Assert;
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest {
+
+    /**
+     * Rigourous Test :-)
+     */
+	 @Test
+	 public void testApp()    {
+        Assert.assertEquals(true, true);
+	 }
+}
--- src/test/java/eu/albertomorales/tddIntro/calculadora/impl/ModuloCalculoImplTest.java
+++ src/test/java/eu/albertomorales/tddIntro/calculadora/impl/ModuloCalculoImplTest.java
@@ -1,63 +1,60 @@
 package eu.albertomorales.tddIntro.calculadora.impl;
 
-import org.testng.annotations.BeforeClass;
-import org.testng.annotations.DataProvider;
-import org.testng.annotations.Test;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import com.tngtech.java.junit.dataprovider.DataProvider;
+import com.tngtech.java.junit.dataprovider.DataProviderRunner;
+import com.tngtech.java.junit.dataprovider.UseDataProvider;
 
 import eu.albertomorales.tddIntro.calculadora.ModuloCalculo;
 
+@RunWith(DataProviderRunner.class)
 public class ModuloCalculoImplTest {
 
-	 @BeforeClass
+	 @Before
 	 public void setUp() {
 	   // code that will be invoked when this test is instantiated
 	 }
 	 
-	 @Test(groups = { "fast" })
-	 public void aFastTest() {
-	   
-	 }
-
-	 @Test(groups = { "fast" })
+	 @Test
 	 public void sumar() {
 		 ModuloCalculo sut = new ModuloCalculoImpl();
 		 Integer resultado = sut.sumar(2,3);
 		 assert resultado == 5;
 	 }
 	 
-	 @DataProvider 
-	 private static final Object[][] sumarMuchos_DP(){
-	     return new Object[][] {
-	         {new Integer(4), new Integer(4), new Integer(8)},
-	         {new Integer(6), new Integer(33), new Integer(39)},
-	         {new Integer(7), new Integer(-14), new Integer(-7)}};
+	 @DataProvider
+	 public static Object[][] sumarMuchos_DP() {
+		 return new Object[][] {
+	         {4, 4, 8},
+	         {6, 33, 40},
+	         {7, -14, -7}
+	     };
 	 }
 	 
-	 @Test(groups = { "fast" }, dataProvider = "sumarMuchos_DP")
-	 public void sumarMuchos(Integer primerSumando, Integer segundoSumando, Integer resultadoEsperado) {
+	 @Test
+	 @UseDataProvider("sumarMuchos_DP")
+	 public void sumarMuchos(int primerSumando, int segundoSumando, int resultadoEsperado) {
 		 ModuloCalculo sut = new ModuloCalculoImpl();
 		 Integer resultado = sut.sumar(primerSumando,segundoSumando);
 		 assert resultado.equals(resultadoEsperado);
 	   
 	 }
-	 
-	 @DataProvider 
-	 private static final Object[][] parametrosNulos_DP(){
-	     return new Object[][] {
-	         {null, new Integer(4)},
-	         {new Integer(6), null}};
-	 }
-	 
-	 @Test(groups = { "fast" }, dataProvider = "parametrosNulos_DP", expectedExceptions = IllegalArgumentException.class)
-	 public void parametrosNulos(Integer primerSumando, Integer segundoSumando) {
+
+	 @Test(expected = IllegalArgumentException.class)
+	 public void primerParametroNulo() {
 		 ModuloCalculo sut = new ModuloCalculoImpl();
-		 sut.sumar(primerSumando,segundoSumando);
+		 sut.sumar(null,4);
 	 }
 
-	 @Test(groups = { "slow" })
-	 public void aSlowTest() {
-	    
+	 @Test(expected = IllegalArgumentException.class)
+	 public void segundoParametroNulo() {
+		 ModuloCalculo sut = new ModuloCalculoImpl();
+		 sut.sumar(6, null);
 	 }
+
 
 	 
 }
